ac28357ffa38f7e5bf5a01aa81f695c63c3c6b15
Wahaj Sajid <mwahajsajidali@gmail.com>
2024-11-25 22:45:58 +0500

    Lecture 0 continue..

---
+++ calculator.py
@@ -0,0 +1,4 @@
+x = input("What's x? ")
+y = input("What's y ")
+z = int(x)+int(y) #converting character to string. Because by default the input will be character. 
+print(z)
--- first.py
+++ first.py
@@ -1,3 +1,8 @@
-name = input("Enter your name: ")
-print("Hello " ,end = "")
-print(name)
+#print your name
+name = input("Enter your name: ").strip().title() #"strip()" will remove the spaces from beginning and ending of string and "title()" will captilize the first letters of all the words in the string 
+#Split the user's name into first and second name
+firstName, secondName = name.split(" ")
+print("Hello" , firstName )
+
+
+
