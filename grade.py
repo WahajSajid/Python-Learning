b3fba06025654e05e2b189385f3e98ae4f13db1d
Wahaj Sajid <mwahajsajidali@gmail.com>
2024-11-26 23:53:06 +0500

    lecture 1 continue

---
+++ grade.py
@@ -0,0 +1,10 @@
+obtainedMarks = float(input("Enter obtained marks: "))
+
+if obtainedMarks>=64 and obtainedMarks<=80:
+    print("Grade: A")
+elif obtainedMarks >=54 and obtainedMarks<64:
+    print("Grade: B")
+elif obtainedMarks >=40 and obtainedMarks<54:
+    print("Grade: C")
+else: 
+    print("Fail")
