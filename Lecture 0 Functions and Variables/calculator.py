b12f139e1d20b7f22e993aeafaba26f504c31940
Wahaj Sajid <141111769+WahajSajid@users.noreply.github.com>
2024-11-30 23:39:36 +0500

    Update calculator.py

--- Lecture 0 Functions and Variables/calculator.py
+++ Lecture 0 Functions and Variables/calculator.py
@@ -1,4 +1,4 @@
 x = float(input("what's x? "))
 y = float(input("what's y? "))
-z = round(x+y)
+z = round(x+y) #round fucntion will round the floting value. We can also spacify the number of floting points we want to round. 
 print(f"{z:,}")
